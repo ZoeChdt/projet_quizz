43e74e2ce5dcb684d39069958821fc47bbbb7ea5
chzoe <ch.zoe68@gmail.com>
2025-11-18 16:34:32 +0100

    creation de la class abstraite question

--- question.h
+++ question.h
@@ -5,9 +5,11 @@
 #ifndef PROJET_QUIZZ_QUESTION_H
 #define PROJET_QUIZZ_QUESTION_H
 
-
 class question {
+public:
+    virtual ~question() = default;
+    virtual string enonce() const =0;
+    virtual bool reponseJuste(const string& reponse) const = 0;
 };
-
 
 #endif //PROJET_QUIZZ_QUESTION_H
