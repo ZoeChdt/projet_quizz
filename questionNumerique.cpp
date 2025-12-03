bce2942f229815ce86070fac1bcf5d1db3b6e772
kenzabakadi <kenzabakadi@gmail.com>
2025-12-03 17:47:21 +0100

    Ajout des limites minimale et maximale dans questionNumérique avec vérification dans le constructeur

--- questionNumerique.cpp
+++ questionNumerique.cpp
@@ -5,9 +5,14 @@
 #include "questionNumerique.h"
 
 #include <utility>
-questionNumerique::questionNumerique(std::string  enonce, int reponse):
-    d_enonce{std::move(enonce)},d_reponse{reponse}
-{}
+questionNumerique::questionNumerique(std::string  enonce, int reponse, int limiteMin, int limiteMax):
+    d_enonce{std::move(enonce)},d_reponse{reponse}, d_limiteMinimum{limiteMin}, d_limiteMaximum{limiteMax}
+{
+    if (d_limiteMinimum > d_limiteMaximum)
+        throw std::invalid_argument("limiteMinimum > limiteMaximum");
+    if (d_reponse < d_limiteMinimum || d_reponse > d_limiteMaximum)
+        throw std::invalid_argument("Réponse hors limites");
+}
 
 std::string questionNumerique::enonce() const {
     return d_enonce;
@@ -15,6 +20,14 @@ std::string questionNumerique::enonce() const {
 
 int questionNumerique::reponse() const {
     return d_reponse;
+}
+
+int questionNumerique::limiteMinimum() const {
+    return d_limiteMinimum;
+}
+
+int questionNumerique::limiteMaximum() const {
+    return d_limiteMaximum;
 }
 
 bool questionNumerique::reponseJuste(const std::string &reponse) const {
--- questionNumerique.h
+++ questionNumerique.h
@@ -10,13 +10,17 @@
 
 class questionNumerique : public question {
 public:
-    questionNumerique(std::string  enonce, int reponse);
+    questionNumerique(std::string  enonce, int reponse, int limiteMin, int limiteMax);
     std::string enonce() const override;
     int reponse() const;
+    int limiteMinimum() const;
+    int limiteMaximum() const;
     bool reponseJuste(const std::string& reponse) const override;
 private:
     std::string d_enonce;
     int d_reponse;
+    int d_limiteMinimum;
+    int d_limiteMaximum;
 };
 
 
